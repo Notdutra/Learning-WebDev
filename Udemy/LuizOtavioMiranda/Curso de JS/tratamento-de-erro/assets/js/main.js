b52201dc7371c7aad0a79a28811916d0a0f42a1d
Arthur S Dutra <arthurdutra@live.ca>
2021-11-16 20:46:17 -0300

    .

--- Udemy/LuizOtavioMiranda/Curso de JS/tratamento-de-erro/assets/js/main.js
+++ Udemy/LuizOtavioMiranda/Curso de JS/tratamento-de-erro/assets/js/main.js
@@ -6,10 +6,17 @@
 
 function soma(x, y) {
   if (typeof x !== "number" || typeof y !== "number") {
-    throw "x e y precisam ser numeros!";
+    //! throw "x e y precisam ser numeros!";  para enviar so esta mensagem
+    throw new Error("x e y precisam ser numeros!");
   }
 
   return x + y;
 }
 
-console.log(soma(1,3));
+try {
+  console.log(soma(1, 3));
+  console.log(soma(1, "Arthur"));
+} catch (error) {
+  console.log(error);
+  //! nao mostrar isso para o usuario!!! sempre colocar algo amigavel sem vazar informacao interna
+}
