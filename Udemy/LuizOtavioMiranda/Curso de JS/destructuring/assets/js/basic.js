92365f8f2dd63060ab1226118520976e437c03ca
Arthur S Dutra <arthurdutra@live.ca>
2021-11-16 17:09:28 -0300

    .

---
+++ Udemy/LuizOtavioMiranda/Curso de JS/destructuring/assets/js/basic.js
@@ -0,0 +1,13 @@
+const numbers = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const [lista1, lista2, lista3] = numbers;
+
+console.log(numbers[0]);
+console.log(`${lista1} \n`);
+
+console.log(numbers[0][0]);
+console.log(`${lista1[0]} \n`);
