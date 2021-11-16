6654abb14bbfea1b3c8f202971668530b3dc7bce
Arthur S Dutra <arthurdutra@live.ca>
2021-11-16 18:05:25 -0300

    .

---
+++ Udemy/LuizOtavioMiranda/Curso de JS/for/assets/js/forIn.js
@@ -0,0 +1,17 @@
+const frutas = ["Abacaxi", "Manga", "Morango", "Limao"];
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
+
+for (let i in frutas) {
+  console.log(frutas[i]);
+}
+
+console.log("\n\n");
+
+const pessoa = {
+  nome: 'Arthur',
+  sobreNome: 'Dutra',
+  idade: 23
+};
