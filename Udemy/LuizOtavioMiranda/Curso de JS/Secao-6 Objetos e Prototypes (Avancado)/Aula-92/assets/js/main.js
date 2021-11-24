b59992562ab6ccf7ed6821c47a598eb7993605ea
Arthur S Dutra <arthurdutra@live.ca>
2021-11-24 04:16:09 -0300

    Update main.js

--- Udemy/LuizOtavioMiranda/Curso de JS/Secao-6 Objetos e Prototypes (Avancado)/Aula-92/assets/js/main.js
+++ Udemy/LuizOtavioMiranda/Curso de JS/Secao-6 Objetos e Prototypes (Avancado)/Aula-92/assets/js/main.js
@@ -1,11 +1,27 @@
-const produto = { nome: 'Caneca', preco: 1.8 };
-const outraCoisaIgual = produto;
-const outraCoisaDiferente = { ...produto };
+//* Ja vimos
+//* Object.keys (retorna as chaves)
+//* Object.freeze (congela o objeto)
+//* Object.defineProperties (define varias propriedades)
+//* Objedct.defineProperty (define uma propriedade)
 
-outraCoisaIgual.nome = 'Outro nome';
-outraCoisaIgual.preco = 2.5;
+const produto = { nome: 'produto', preco: 1.8 };
+Object.defineProperty(produto, 'nome', {
+  writable: false,
+  configurable: false,
+  value: 'Qualquer outra coisa', //! Aqui eu posso mudar
+});
 
-outraCoisaDiferente.nome = 'Nome unico';
-outraCoisaDiferente.preco = 4.5;
-console.log(produto);
-console.log(outraCoisaDiferente);
+console.log(Object.getOwnPropertyDescriptor(produto, 'nome'));
+
+produto.nome = 'Outra coisa';
+console.log(produto); //! Nao alterou por causa do writable
+
+delete produto.nome;
+console.log(produto); //! Nao deletou por causa do configurable
+
+// delete produto.preco;
+// console.log(produto); //! Alterou
+console.log('\n\n');
+console.log(Object.values(produto)); //* Retorna os valores
+console.log(Object.keys(produto)); //* Retorna as chaves
+console.log(Object.entries(produto)); //* Retorn em array [key, value]
