6734b4a3e5cd490e8a8f95a0f91b05e42ac8af87
Arthur S Dutra <arthurdutra@live.ca>
2021-11-24 13:35:19 -0300

    Update main.js

--- Udemy/LuizOtavioMiranda/Curso de JS/Secao-6 Objetos e Prototypes (Avancado)/Aula-99/assets/js/main.js
+++ Udemy/LuizOtavioMiranda/Curso de JS/Secao-6 Objetos e Prototypes (Avancado)/Aula-99/assets/js/main.js
@@ -2,4 +2,31 @@
 //* Classes = funcao construtora
 
 //! Superclass
-function Conta(agencia, conta, saldo) {}
+function Conta(agencia, conta, saldo) {
+  this.agencia = agencia;
+  this.conta = conta;
+  this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function (valor) {
+  if (this.saldo <= valor) {
+    this.verSaldo();
+    return;
+  }
+  this.saldo -= valor;
+};
+Conta.prototype.depositar = function (valor) {
+  this.saldo += valor;
+  this.verSaldo();
+};
+Conta.prototype.verSaldo = function () {
+  console.log(
+    `Agencia: ${this.agencia}/${this.conta} - Saldo: ${this.saldo.toFixed(2)}`
+  );
+};
+
+const conta1 = new Conta(11, 2201, 0);
+
+console.log(conta1);
+conta1.depositar(1000);
+console.log(conta1);
