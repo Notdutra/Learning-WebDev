697cc8742e6ef44b20ebc382dcfcb6bf3f9974fa
Arthur S Dutra <arthurdutra@live.ca>
2021-11-29 13:57:38 -0300

    Heranca com Classes

--- Udemy/LuizOtavioMiranda/Curso de JS/Secao-7 Programacao Orientada a Objetos/Aula-104/assets/js/main.js
+++ Udemy/LuizOtavioMiranda/Curso de JS/Secao-7 Programacao Orientada a Objetos/Aula-104/assets/js/main.js
@@ -1,27 +1,59 @@
 //* Herança com classes
-console.log('Hello');
+class DispositivoEletronico {
+  constructor(nome) {
+    this.nome = nome;
+    this.ligado = false;
+  }
 
-class DispositivoEletronico() {
-    constructor(nome) {
-        this.nome = nome;
-        this.ligado = false;
+  ligar() {
+    if (this.ligado) {
+      console.log(`${this.nome} ja esta ligado`);
+      return;
     }
 
-    ligar() {
-        if (this.ligado) {
-            console.log(`${this.nome} ja esta ligado`);
-            return;
-        }
+    this.ligado = true;
+  }
 
-        this.ligado = true;
+  desligar() {
+    if (!this.ligado) {
+      console.log(`${this.nome} ja esta desligado`);
+      return;
     }
 
-    desligar() {
-        if (!this.ligado) {
-            console.log(`${this.nome} ja esta desligado`);
-            return;
-        }
-
-        this.ligado = false;
-    }
+    this.ligado = false;
+  }
 }
+
+class SmartPhone extends DispositivoEletronico {
+  constructor(nome, cor, modelo) {
+    super(nome);
+
+    this.cor = cor;
+    this.modelo = modelo;
+  }
+}
+
+class Tablet extends DispositivoEletronico {
+  constructor(nome, temWifi) {
+    super(nome);
+    this.temWifi = temWifi;
+  }
+
+  ligar() {
+    //! Sobreescreveu o metodo ligar da classe DispositivoEletronico
+    console.log('Olha, voce alterou o metodo ligar');
+  }
+
+  falaOi() {
+    //! Pertenca apenas a classe Tablet
+    console.log('Oi');
+  }
+}
+
+const s1 = new SmartPhone('Samsung', 'Azul', 'Galaxy S20');
+console.log(s1);
+
+console.log();
+
+const t1 = new Tablet('iPad', true);
+console.log(t1);
