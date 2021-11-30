5bd3b411cbb4f0e4c6e21c3bdaeff75a64195f51
Arthur S Dutra <arthurdutra@live.ca>
2021-11-30 18:27:46 -0300

    Alguns metodos de Promise

--- Udemy/LuizOtavioMiranda/Curso de JS/Secao-8 Assincrono - Promises, Ajax, Fetch e Async Await/Aula-108/assets/js/main.js
+++ Udemy/LuizOtavioMiranda/Curso de JS/Secao-8 Assincrono - Promises, Ajax, Fetch e Async Await/Aula-108/assets/js/main.js
@@ -1,4 +1,4 @@
-//* Promises
+//* Metodos para Promises
 function rand(min, max) {
   min *= 1000;
   max *= 1000;
@@ -7,28 +7,76 @@ function rand(min, max) {
 
 function esperaAi(msg, tempo) {
   return new Promise((resolve, reject) => {
-    if (typeof msg !== 'string') reject('BAD VALUE');
+    if (typeof msg !== 'string') {
+      reject('CAI NO ERRO');
+      return;
+    };
+
     setTimeout(() => {
-      resolve(msg);
+      resolve(msg.toUpperCase() + ' - Passei na promise');
     }, tempo);
   });
 }
 
-esperaAi('conexao com o BD', rand(1, 3))
-  .then(response => {
-    console.log(response);
-    return esperaAi('Buscando dados da BASE', rand(1, 3));
-  })
-  .then(response => {
-    console.log(response);
-    return esperaAi('Tratando os dados da BASE', rand(1, 3));
-  }).then(response => {
-    console.log(response);
-  }).then(() => {
-    console.log('Exibe dados na tela');
-  })
-  .catch(e => {
-    console.log('Erro:', e);
-  });
+//* Promise.all, Promise.race, Promise.resolve, Promise.reject
 
-console.log('Isso eh exibido antes de qualquer promise');
+//! Promise.all usa e retona um array com varias promises
+const promises = [
+  'Primeiro valor',
+  esperaAi('Promise 1', 3000),
+  esperaAi('Promise 2', 500),
+  esperaAi('Promise 3', 1000),
+  // esperaAi(4, 1000), //! Erro em alguma das promises, retorna falso
+
+  'Outro valor'
+];
+
+// Promise.all(promises)
+//   .then(function (valor) {
+//     console.log(valor);
+//   })
+//   .catch(function (erro) {
+//     console.log(erro);
+//   });
+
+
+//! a primeira que resolver ele retorna e para, no caso ele retorna primeiro valor, pois ja esta pronto, se nao tivesse entao ele retornaria 'Promise 2' ja que ela demora meio segundo
+// Promise.race(promises)
+//   .then(function (valor) {
+//     console.log(valor);
+//   })
+//   .catch(function (erro) {
+//     console.log(erro);
+//   });
+
+function baixaPaginaResolve() {
+  const emCache = true;
+
+  if (emCache) {
+    return Promise.resolve('Pagina em cache');
+  } else {
+    return esperaAi('Baixei a pagina', 3000);
+  }
+}
+
+function baixaPaginaReject() {
+  const emCache = true;
+
+  if (emCache) {
+    return Promise.reject('Pagina em cache');
+  } else {
+    return esperaAi('Baixei a pagina', 3000);
+  }
+}
+
+baixaPaginaResolve()
+  .then(dadosPagina => {
+    console.log(dadosPagina);
+  })
+  .catch(e => console.log('Erro', e));
+
+baixaPaginaReject()
+  .then(dadosPagina => {
+    console.log(dadosPagina);
+  })
+  .catch(e => console.log('Erro', e));
